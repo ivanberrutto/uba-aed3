3bae35b42709dcb6dfbe59a217aaf93420648e99
Ivan Berrutto <ivanberrutto@gmail.com>
2024-05-11 18:04:41 -0300

    resuelvo guia 2 ej 2

--- Guias de Ejercicios/Guia 2.py
+++ Guias de Ejercicios/Guia 2.py
@@ -1,20 +1,41 @@
 
 # T(n) = a T(n/c) + f(n)
+#PD: Estos ejercicios no estan testeados para compilar, son puramente de uso practico - pseudocodigo.
 
 #Ejercicio 1:
 def IzquierdaDominante(A):
 	if len(A) == 1:
 		return True
 
-	if(sum(A[:len(A)//2])> sum(A[len(A)//2:]) && IzquierdaDominante(A[:len(A)//2]) ):
-		return True
-	else:
-		return False
+	return ( sum(A[:len(A)//2]) > sum(A[len(A)//2:]) ) and IzquierdaDominante(A[:len(A)//2])
 
-# complejidad o(n log n)?
+		
 
 # a = 1
 # c = 1
-# f(n) = n/2 
+# f(n) = on
 
-# por tm 2° complejidad: n**(log1 1) log n = n * log n
+# por tm 3° complejidad= n
+
+#Ejercicio 2:
+
+def IndiceEspejo(A,p=0,f=None):
+	#print("p es ",p," f es ",f)
+	if f is None:
+		f = len(A) - 1
+	m = (p+f) //2
+	if A[m] == m:
+		return True
+	if p==f:
+		return False
+	if m>A[m]:
+		return IndiceEspejo(A,m+1,f)
+	else:
+		return IndiceEspejo(A,p,m-1)
+
+print(IndiceEspejo([0, -1, 5, 10, 7]))
+
+# a = 1
+# c = 1
+# f(n) = o1
+# por tm 2° complejidad: n**( log1 1 = 0 ) log n =  log n
