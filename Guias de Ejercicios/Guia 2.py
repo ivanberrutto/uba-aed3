8ea5f2719769efad2df12b1ed47cb258bddcb39b
Ivan Berrutto <ivanberrutto@gmail.com>
2024-05-11 18:49:51 -0300

    resuelvo guia 2 ej 3

--- Guias de Ejercicios/Guia 2.py
+++ Guias de Ejercicios/Guia 2.py
@@ -33,9 +33,49 @@ def IndiceEspejo(A,p=0,f=None):
 	else:
 		return IndiceEspejo(A,p,m-1)
 
-print(IndiceEspejo([0, -1, 5, 10, 7]))
+#print(IndiceEspejo([0, -1, 5, 10, 7]))
 
 # a = 1
 # c = 1
+# f(n) = o1
+# por tm 2° complejidad: n**( log1 1 = 0 ) log n =  log n
+
+
+#Ejercicio 3:
+
+#forma sin D&Q 
+def PotenciaLogaritmicaSinDQ(a,b,aActual = 0 , n=1):
+	if(aActual==0 and a!=0):
+		aActual=a
+	if(b==0):
+		return 1
+	if(b==1):
+		return a 
+	if(n>=b):
+		return  PotenciaLogaritmica(a, b)
+
+	return aActual * PotenciaLogaritmica(a , b - n , aActual*aActual , n * 2)  
+
+
+
+# Forma D&Q
+def PotenciaLogaritmica(a, b):
+    if b == 0:
+        return 1
+    if b == 1:
+        return a
+    mitad = PotenciaLogaritmica(a, b // 2)
+    
+    # Si b es par
+    if b % 2 == 0:
+        return mitad * mitad
+    else:
+        # Si b es impar, tenemos que multiplicar una vez más por a
+        return mitad * mitad * a
+
+#print(PotenciaLogaritmica(5,4))
+
+# a = 1
+# c = 2
 # f(n) = o1
 # por tm 2° complejidad: n**( log1 1 = 0 ) log n =  log n
