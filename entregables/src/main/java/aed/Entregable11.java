6a4092eeb48dd1c949757e61457623292fba60ce
Ivan Berrutto <ivanberrutto@gmail.com>
2024-06-08 01:37:35 -0300

    subo entregable 12

---
+++ entregables/src/main/java/aed/Entregable11.java
@@ -0,0 +1,27 @@
+package aed;
+
+import java.util.Scanner;
+
+public class Entregable11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            int n = scanner.nextInt();
+            if (!scanner.hasNext()){
+                break;
+            }
+            int m = scanner.nextInt();
+            for (int i = 0; i < m; i++) {
+                int si = scanner.nextInt();
+                int ni = scanner.nextInt();
+                String oi = scanner.next();
+                int ki = scanner.nextInt();
+
+
+
+             }
+
+
+        }
+    }
+}
